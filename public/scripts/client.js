4623ed9df96fb0444abebb920e3cb50e1c670845
Matija <matija_bozic@yahoo.com>
2021-12-16 02:35:27 +0000

    fetching tweets has been implemented

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,31 +6,7 @@
 
 // Fake data taken from initial-tweets.json
 $(document).ready(function() {
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
+  
 const renderTweets = function (tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
@@ -68,15 +44,21 @@ const createTweetElement = function(tweet) {
   return $tweet;
 };
 
-renderTweets(tweetData);
+const loadTweets = function () {
+  $.ajax('/tweets', {
+    dataType: 'json', 
+    method: 'GET', 
+  })
+    .then((result) => {
+      renderTweets(result);
+    });
+};
+
+loadTweets();
 
 $("form").on("submit", function (event) {
   event.preventDefault();
   let url = '/tweets/';
-  $.ajax({
-    urlurl: url,
-    method: "POST",
-    data: $(this).serialize();
-  });
+  $.post(url, $(this).serialize());
  });
 });
