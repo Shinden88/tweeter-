6dc0f3a72dd496e4074f28ea168dcc72f8a49b13
Matija <matija_bozic@yahoo.com>
2021-12-17 01:28:18 +0000

    added go-up button and reorganized the files

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   $("#tweet-text").on("input", function(event) {
     const charsLeft = 140 - $(this).val().length; 
+   
     let counter = $(this).siblings().children(".counter").text(charsLeft);
     
     if (charsLeft < 0) {
