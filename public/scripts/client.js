549933c3c95c70129052218dee1a46c62bfe9783
Matija <matija_bozic@yahoo.com>
2022-01-04 05:26:36 +0000

    fixed code so it displays new tweets without refreshing

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,10 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
 $(document).ready(function () {
+
   dayjs().format();
   dayjs.extend(window.dayjs_plugin_relativeTime);
+ 
   const renderTweets = function (tweets) {
     $('#tweets').empty();
     // loops through tweets
@@ -20,16 +21,18 @@ $(document).ready(function () {
   };
 
   const escape = function (str) {
-    const div = document.createElement('div');
+    let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
   const createTweetElement = function (tweet) {
+
     const time = dayjs(tweet.created_at).fromNow();
+
     let $tweet = /* Your code for creating the tweet element */
-  // ...
-  `<article class="tweet" id="tweetz">
+      // ...
+      `<article class="tweet" id="tweetz">
           <header>
             <div class="image-username">
               <img src="${tweet.user.avatars}">
@@ -51,36 +54,51 @@ $(document).ready(function () {
   };
 
   const loadTweets = function () {
+
     $.ajax('/tweets', {
-      dataType: 'json', 
-      method: 'GET', 
+      dataType: 'json',
+      method: 'GET'
     })
       .then((result) => {
         renderTweets(result);
         return;
       });
-  };  
-
+  };
   loadTweets();
 
-  $('form').on('submit', function (event) {
+  $("form").on("submit", function (event) {
     event.preventDefault();
+
     const data = $('#tweet-text').val();
+
     if (data.trim() === '') {
-      $('#empty').slideDown();
+      $("#empty").slideDown();
       return;
-    } if (data.length > 140) {
-      $('#error').slideDown();
+    } else if (data.length > 140) {
+      $("#error").slideDown();
       return;
     }
     let url = '/tweets/';
     $.ajax({
-      url: url,
-      method: 'POST',
+      type: "POST",
+      url: "/tweets",
       data: $(this).serialize(),
-    })
-      .then(loadTweets());
-    $(this).children('.submit-tweet').children('.counter').val('140');
-    $('#tweet-text').val('');
+      success: function() {
+        loadTweets();               //if success load tweets and make text area empty,counter back to 140
+        $('#tweet-text').val('');
+        $('.counter').val("140");
+      },
+    });
+
+    // $.ajax({
+    //   url: url,
+    //   method: "POST",
+    //   data: $(this).serialize(),
+      
+    // })
+    //   .then(loadTweets());
+    // $(this).children('.submit-tweet').children('.counter').val('140');
+    // $('#tweet-text').val('');
+
   });
 });
